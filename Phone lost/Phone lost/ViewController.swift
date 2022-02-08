07f786f75a6bd059de87a6ba7698436dc199e370
acepele <47071340+acepele@users.noreply.github.com>
2022-02-07 20:19:35 -0600

    Add files via upload

---
+++ Phone lost/Phone lost/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Phone lost
+//
+//  Created by student on 2/7/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor.systemPink       // Do any additional setup after loading the view.
+    }
+
+
+}
+
